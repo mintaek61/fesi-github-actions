a1efd04e23851bf963acc97f3f5f018f20af6e4d
mintaek <omt610@gmail.com>
2025-11-11 09:48:39 +0900

    framer motion 간단 실습1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,13 @@
-"use client";
-
-import { useState } from "react";
+import * as motion from "motion/react-client";
 
 export default function Home() {
-  const [isFlipped, setIsFlipped] = useState(false);
-
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div
-        className="cursor-pointer perspective-midrange"
-        onClick={() => setIsFlipped(!isFlipped)}
-      >
-        <div
-          className={`relative h-96 w-64 transition-transform duration-700 transform-3d ${
-            isFlipped ? "rotate-y-180" : ""
-          }`}
-        >
-          <div className="absolute flex h-full w-full items-center justify-center rounded-xl bg-white p-4 shadow-lg backface-hidden">
-            <div>카드앞면</div>
-          </div>
-          <div className="absolute flex h-full w-full rotate-y-180 items-center justify-center rounded-xl bg-blue-500 p-4 text-white shadow-lg backface-hidden">
-            <div>카드뒷면</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      안녕하세요! 페이드인 효과입니다.
+    </motion.div>
   );
 }

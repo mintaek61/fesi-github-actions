fb3cbfee8bd7276dbb86459084fba6c584233d0f
mintaek <omt610@gmail.com>
2025-11-11 09:24:55 +0900

    반응형 실습2: 네비게이션바 반응형 적용하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,33 @@
-import Image from "next/image";
+import Link from "next/link";
 
-type Product = {
-  id: number;
-  title: string;
-  price: number;
-  category: string;
-  description: string;
-  image: string;
-};
+function Hamburger() {
+  return (
+    <svg
+      className="h-6 w-6 text-gray-800"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 17 14"
+    >
+      <path d="M16 2H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 0 1 0 2Z" />
+    </svg>
+  );
+}
 
 export default async function Home() {
-  // 무료 상품 API 문서: https://fakestoreapi.com/docs
-  const response = await fetch("https://fakestoreapi.com/products");
-  const products = await response.json();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {products.map((product: Product) => (
-        <div key={product.id} className="border shadow-sm">
-          <Image
-            src={product.image}
-            alt={product.title}
-            width={200}
-            height={200}
-          />
-          <h1>{product.title}</h1>
-          <p>{product.price}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      {/* 데스크탑 버전 메뉴 */}
+      <div className="hidden items-center gap-4 md:flex">
+        <Link href="/">코드잇 단기심화</Link>
+        <Link href="/bootcamp">부트캠프</Link>
+        <Link href="/job-support">취업 지원</Link>
+        <Link href="/blog">후기&블로그</Link>
+      </div>
+      {/* 모바일 버전 메뉴 */}
+      <div className="block md:hidden">
+        <Hamburger />
+      </div>
+    </>
   );
 }

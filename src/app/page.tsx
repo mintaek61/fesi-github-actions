ea61fb6651a1bdccc88e18dbbc6918e33719490f
mintaek <omt610@gmail.com>
2025-11-11 09:25:33 +0900

    반응형 실습3: 상품 상세 반응형 적용하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,19 @@
-import Link from "next/link";
-
-function Hamburger() {
-  return (
-    <svg
-      className="h-6 w-6 text-gray-800"
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      viewBox="0 0 17 14"
-    >
-      <path d="M16 2H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 0 1 0 2Z" />
-    </svg>
-  );
-}
+import Image from "next/image";
 
 export default async function Home() {
+  const response = await fetch("https://fakestoreapi.com/products/1");
+  const product = await response.json();
+
   return (
-    <>
-      {/* 데스크탑 버전 메뉴 */}
-      <div className="hidden items-center gap-4 md:flex">
-        <Link href="/">코드잇 단기심화</Link>
-        <Link href="/bootcamp">부트캠프</Link>
-        <Link href="/job-support">취업 지원</Link>
-        <Link href="/blog">후기&블로그</Link>
+    <div className="items-center md:flex">
+      <Image src={product.image} alt={product.title} width={200} height={200} />
+      <div>
+        <h1 className="font-bold md:text-2xl">{product.title}</h1>
+        <p className="text-sm text-gray-500 md:text-lg">
+          {product.description}
+        </p>
+        <p className="font-bold md:text-2xl">$ {product.price}</p>
       </div>
-      {/* 모바일 버전 메뉴 */}
-      <div className="block md:hidden">
-        <Hamburger />
-      </div>
-    </>
+    </div>
   );
 }

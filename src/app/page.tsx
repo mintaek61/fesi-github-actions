14fb2a5213a6cbfe8763793cc7751abdcbd4cd8d
mintaek <omt610@gmail.com>
2025-11-11 09:47:35 +0900

    테일윈드 애니메이션 실습: 카드 뒤집기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function ShowUpExample() {
+"use client";
+
+import { useState } from "react";
+
+export default function Home() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
-    <div className="flex h-screen items-center justify-center gap-2">
-      <div className="animate-show-up h-96 w-72 rounded-lg border shadow-md"></div>
+    <div className="flex min-h-screen items-center justify-center">
+      <div
+        className="cursor-pointer perspective-midrange"
+        onClick={() => setIsFlipped(!isFlipped)}
+      >
+        <div
+          className={`relative h-96 w-64 transition-transform duration-700 transform-3d ${
+            isFlipped ? "rotate-y-180" : ""
+          }`}
+        >
+          <div className="absolute flex h-full w-full items-center justify-center rounded-xl bg-white p-4 shadow-lg backface-hidden">
+            <div>카드앞면</div>
+          </div>
+          <div className="absolute flex h-full w-full rotate-y-180 items-center justify-center rounded-xl bg-blue-500 p-4 text-white shadow-lg backface-hidden">
+            <div>카드뒷면</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

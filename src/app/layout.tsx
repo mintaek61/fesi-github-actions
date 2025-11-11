815e97d301a9736a14c90c6cd7b2557bd459be65
mintaek <omt610@gmail.com>
2025-11-11 09:18:18 +0900

    다크모드 실습2: 수동 다크모드

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// import { ThemeProvider } from "@/providers/ThemeProvider";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 import "./globals.css";
 
 export default function RootLayout({
@@ -9,8 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
-        {/* <ThemeProvider>{children}</ThemeProvider> */}
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );

e44ae777f0c1c75a6c60fd2787798326bac5f898
mintaek <omt610@gmail.com>
2025-11-19 17:08:36 +0900

    pull request 생성 시 actions 실행

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,28 +1,5 @@
 // src/app/page.test.tsx
 
-import { render, screen } from "@testing-library/react";
-import Home from "./page";
-
-describe("홈페이지 테스트", () => {
-  test("스토리북 버튼이 렌더링되는지 테스트", () => {
-    render(<Home />);
-
-    // 버튼이 화면에 표시되는지 확인
-    const button = screen.getByRole("button", { name: "스토리북 버튼" });
-    expect(button).toBeInTheDocument();
-  });
-
-  // test("네트워크 에러 발생 시 모킹 테스트", async () => {
-  //   server.use(
-  //     http.get("http://localhost:4000/posts/1", () => {
-  //       return HttpResponse.json(null, { status: 500 });
-  //     }),
-  //   );
-
-  //   render(<Home />);
-  //   const errorMessage = await screen.findByText(
-  //     "서버에서 데이터를 가져오는 데 실패했습니다.",
-  //   );
-  //   expect(errorMessage).toBeInTheDocument();
-  // });
+test("실패하는 테스트", () => {
+  expect(true).toBe(false);
 });
